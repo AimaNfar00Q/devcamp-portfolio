a65ae6f2ef308cc6ff75452d7d80d0fc9e864de4
Aiman Farooq <owais.raza@gomoment.com>
2021-12-06 18:09:38 +0500

    Integrated NEW and CREATE actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,23 @@ class PortfoliosController < ApplicationController
 	def index
 	@portfolio_items = Portfolio.all #calling from portfolio model
 	end
+
+	def new
+		@portfolio_item= Portfolio.new
+	end
+
+	def create
+	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body)) #getting the parameter of form title nd body
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio item was successfully created." } #will redirect to blog show page
+        
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        
+     end
+   end	
+ end
+
 end
