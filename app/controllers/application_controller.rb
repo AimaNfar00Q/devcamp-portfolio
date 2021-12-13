5a0398f6d68df2c09359bc7bb6c8104b8be9c874
Aiman Farooq <owais.raza@gomoment.com>
2021-12-13 18:20:21 +0500

    added NEW PARAMETERS for user Registration and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+before_action :configure_permitted_parameters, if: :devise_controller?  #through this we'll be able to get the value of customized attr name in upfdate and signup form
+def configure_permitted_parameters
+	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+end
 end
