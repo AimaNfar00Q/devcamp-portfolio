cc7ce9ceed72e4b45315a4ed3bc7675af11c5be3
Aiman Farooq <owais.raza@gomoment.com>
2021-12-15 13:27:42 +0500

    Implemented VIEW HELPER for Auth links register login logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+# def sample_helper  this methd will return smthng to return both yay and something do concat
+#     x=2
+#  if x==1
+#  	"no"
+#  else
+#  	"yay"
+#  	"something"
+#  end
+# 	 end
+
+
+def login_helper
+   if current_user.is_a?(User) 
+    link_to "logout",destroy_user_session_path, method: :delete 
+    else 
+    (link_to "register",new_user_registration_path ) +
+     "<br>".html_safe +
+    ( link_to "login",new_user_session_path )
+    
+    end 
+  end
 end
